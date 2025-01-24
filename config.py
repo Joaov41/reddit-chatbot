85857c2b2ae2cb3346e0fb2d9088b558034ea203
Joaov41 <52511571+Joaov41@users.noreply.github.com>
2025-01-24 19:58:36 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Reddit API Credentials
+REDDIT_CLIENT_ID = ''
+REDDIT_CLIENT_SECRET = ''
+REDDIT_USER_AGENT = 'Reddit Hot Posts Extractor'
+
+
+# Gemini API key
+GEMINI_API_KEY = ""  # Add Gemini API key
