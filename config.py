232fdb93bb068673021b35a8e78c7d467b164fe0
Joaov41 <52511571+Joaov41@users.noreply.github.com>
2025-01-24 19:55:03 +0000

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-
-# Reddit API Credentials
-REDDIT_CLIENT_ID = 'your_reddit_client_id'
-REDDIT_CLIENT_SECRET = 'your_reddit_client_secret'
-REDDIT_USER_AGENT = 'reddit user agent'
-
-# OpenAI API Key
-OPENAI_API_KEY = 'your_openai_api_key'
